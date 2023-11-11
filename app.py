3bde0899644d123f7b1088a1eefcd5f9ff2c9b2c
tenchanhe <chanhe0719@gmail.com>
2023-11-12 00:38:51 +0800

    js recorder

--- app.py
+++ app.py
@@ -1,24 +1,38 @@
-from flask import Flask, request, Response, stream_with_context
+import whisper
+from flask import Flask, request, Response, stream_with_context, render_template
 from main import CourseLangChain
  
 app = Flask(__name__)
 
-@app.route('/api/ask', methods=['GET'])
-def main():
-  args = request.args
-  question = args.get("question")
-  # Lock here
-  if question and chain.handler.finish:
-    chain.handler.finish = False
-    chain.handler.tokens = []
-    return Response(stream_with_context(chain.query(question)), mimetype='text/event-stream')
-  else:
-    return Response("None", mimetype="text/html")
+def chain_run(query):
+  chain = CourseLangChain(cli=True)
+  while True:
+    print(query)
+    print("Bot:")
+    chain.chain.run(query)
 
-if __name__ == "__main__":
-  try:
-    chain = CourseLangChain()
-    app.run(port=59014)
-  finally:
-    print("Deleting chain...")
-    del chain
+
+@app.route('/')
+def base():
+    return render_template('index.html')
+
+
+@app.route('/save_sound', methods=['POST'])
+def save_sound_route():
+    sound = request.files['sound']
+    sound.save("sound.wav")
+    return "录音已保存"
+
+
+@app.route('/convert_audio')
+def convert_audio():
+    # 在这里进行音频转文本的处理，已经将转换后的文本保存在变量 text 中
+    model = whisper.load_model("base")
+    result = model.transcribe("sound.wav")
+    text = result["text"]
+    chain_run(text)
+    return text
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
---
+++ static/js/recorder.js
@@ -0,0 +1,69 @@
+const recordBtn = document.querySelector(".record-btn");
+const player = document.querySelector(".audio-player");
+
+if (navigator.mediaDevices.getUserMedia) {
+  var chunks = [];
+  const constraints = { audio: true };
+  navigator.mediaDevices.getUserMedia(constraints).then(
+    stream => {
+      console.log("授权成功！");
+
+      const mediaRecorder = new MediaRecorder(stream);
+
+      recordBtn.onclick = () => {
+        if (mediaRecorder.state === "recording") {
+          mediaRecorder.stop();
+          recordBtn.textContent = "record";
+          console.log("录音结束");
+        } else {
+          mediaRecorder.start();
+          console.log("录音中...");
+          recordBtn.textContent = "stop";
+        }
+        console.log("录音器状态：", mediaRecorder.state);
+      };
+
+      mediaRecorder.ondataavailable = e => {
+        chunks.push(e.data);
+      };
+
+      mediaRecorder.onstop = e => {
+        var blob = new Blob(chunks, { type: "audio/ogg; codecs=opus" });
+        chunks = [];
+        var audioURL = window.URL.createObjectURL(blob);
+        player.src = audioURL;
+
+          // 将录音数据发送到服务器保存
+          var formData = new FormData();
+          formData.append("sound", blob, "sound.wav");
+          fetch("/save_sound", {
+            method: "POST",
+            body: formData
+          })
+          .then(response => {
+            console.log("录音已保存到服务器");
+            // 请求服务器转换文本，并将转换后的文本显示在网页中
+            fetch("/convert_audio")
+              .then(response => response.text())
+              .then(text => {
+                const textOutput = document.querySelector(".text-output");
+                textOutput.textContent = text;
+                console.log("录音已转换为文本并显示在网页中");
+              })
+              .catch(error => {
+                console.error("转换文本时出错：", error);
+              });
+          })
+          .catch(error => {
+            console.error("保存录音时出错：", error);
+          });
+      };
+    },
+    
+    () => {
+      console.error("授权失败！");
+    }
+    );
+} else {
+  console.error("浏览器不支持 getUserMedia");
+}
