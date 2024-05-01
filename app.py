072c8fc46254fa9ad17e8fb59d42af93ceedb4d2
andyjjrt <andyjjrt@gmail.com>
2024-05-01 18:41:56 +0800

    fix: remove whisper for better performance

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import os
 import uuid
-import whisper
+# import whisper
 from flask import Flask, request, Response, stream_with_context, jsonify, abort
 from main import CourseLangChain
 
@@ -17,33 +17,34 @@ def main():
   if question and chain.handler.finish:
     chain.handler.finish = False
     chain.handler.tokens = []
-    return Response(stream_with_context(chain.query(question)), mimetype='text/event-stream')
+    return Response(stream_with_context(chain.invoke(question)), mimetype='text/event-stream')
   else:
     return Response("None", mimetype="text/html")
 
-@app.route('/api/upload', methods=['POST'])
-def upload_file():
-  if request.method == 'POST':
-    # check if the post request has the file part
-    if 'file' not in request.files:
-      abort(500, "No file part")
-    file = request.files['file']
-    if file.filename == '':
-      abort(500, "No selected file")
-    id = uuid.uuid4()
-    file.save(os.path.join(app.config['UPLOAD_FOLDER'], f"{id}.mp3"))
-    result = model.transcribe(os.path.join(app.config['UPLOAD_FOLDER'], f"{id}.mp3"))
-    return {"text": result["text"]}
+# @app.route('/api/upload', methods=['POST'])
+# def upload_file():
+#   if request.method == 'POST':
+#     # check if the post request has the file part
+#     if 'file' not in request.files:
+#       abort(500, "No file part")
+#     file = request.files['file']
+#     if file.filename == '':
+#       abort(500, "No selected file")
+#     id = uuid.uuid4()
+#     file.save(os.path.join(app.config['UPLOAD_FOLDER'], f"{id}.mp3"))
+#     result = model.transcribe(os.path.join(app.config['UPLOAD_FOLDER'], f"{id}.mp3"))
+#     return {"text": result["text"]}
 
 @app.errorhandler(500)
 def custom400(error):
     return jsonify({'message': error.description})
 
 if __name__ == "__main__":
+  chain = None
   try:
-    model = whisper.load_model("medium")
+    # model = whisper.load_model("medium")
     chain = CourseLangChain()
-    app.invoke(port=59014)
+    app.run(port=59014)
   finally:
     print("Deleting chain...")
     del chain
